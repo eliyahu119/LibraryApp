8fd44cf0979ceb484f2f6d42cafdaf3ce12dfd9d
eliyahu <eliyahu299@gmail.com>
2021-04-01 11:24:08 +0300

    select all books command

---
+++ libaryApp/SqlCommands/SelectAllBooks.sql
@@ -0,0 +1,4 @@
+﻿select b.bookname, Genres.Genre, Authors.Author, Publishers.Publisher ,b.PublicationYear from Books b 
+left join Genres on  b.GenreID=Genres.GenreID
+left join Publishers on  Publishers.PublisherID=b.PublisherID
+left join Authors on Authors.AuthorID=b.AuthorID where b.GenreID=16 
