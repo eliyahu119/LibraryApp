c21dad353f507d31c9bbc2640614f2b65add3266
eliyahu <eliyahu299@gmail.com>
2021-04-02 16:26:26 +0300

    updated sql script

--- libaryApp/SqlCommands/SelectAllBooks.sql
+++ libaryApp/SqlCommands/SelectAllBooks.sql
@@ -1,4 +1,4 @@
 ﻿select b.bookname, Genres.Genre, Authors.Author, Publishers.Publisher ,b.PublicationYear from Books b 
 left join Genres on  b.GenreID=Genres.GenreID
 left join Publishers on  Publishers.PublisherID=b.PublisherID
-left join Authors on Authors.AuthorID=b.AuthorID where b.GenreID=16 
+left join Authors on Authors.AuthorID=b.AuthorID 
